f1dea2fca6fa6ade741fd281d6e46915e0667fee
grynka <ihor.kozhemyakin@gmail.com>
2022-10-21 19:47:33 +0300

    upd

--- src/components/FriendList.js
+++ src/components/FriendList.js
@@ -1,9 +1,27 @@
+const ofline = {
+  display: "block",
+  width: 10,
+  height: 10,
+  borderRadius: "50%",
+  backgroundColor: "red", 
+  margin: "0 15px"
+};
+
+const online = {
+  display: "block",
+  width: 10,
+  height: 10,
+  borderRadius: "50%",
+  backgroundColor: "green", 
+  margin: "0 15px"
+};
+
 function FriendList({ items }) {
     return (
       <ul className="friend-list">
     {items.map(item =>
 <li className="item" key={item.id}>
-  <span className="status" style={{backgroundColor: {item.isOnline ? "green" : "red"}}} ></span>
+  <span className="status" style={item.isOnline ? online : ofline} ></span>
   <img className="avatar" src={item.avatar} alt="User avatar" width="48" />
   <p className="name">{item.name}</p>
 </li>)}
--- src/components/Stastics.js
+++ src/components/Stastics.js
@@ -1,11 +1,17 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
+
 function Statistics({ items }) { return (
 <section className="statistics">
 <h2 className="title">Upload stats</h2>
 <ul className="stat-list">
 {items.map(item => 
-    <li key={item.id}>
+    <li key={item.id} style = {{backgroundColor: getRandomHexColor()}}>
     <span className="label">{item.label}</span>
-    <span className="percentage">{item.itempercentage}%</span>
+    <span className="percentage">{item.percentage}%</span>
     </li> )}
     </ul>
 </section>
